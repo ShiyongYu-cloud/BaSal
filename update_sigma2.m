7e507fc2aba23e3d743ecd590bc880ff2721c266
Shiyong Yu <66739293+ShiyongYu-cloud@users.noreply.github.com>
2024-01-24 09:18:43 -0600

    Update update_sigma2.m

--- update_sigma2.m
+++ update_sigma2.m
@@ -8,10 +8,16 @@ function sigma2_out = update_sigma2(S,phi,DT)
 % sigma2_out: new state of sigma2
 %%
 N = numel(S)-1; %number of time grids
-alpha = (N-1)/2;
+alpha = (N+1)/2;
 beta = 0;
-for i = 2:N
-    beta = beta + (S(i)-(S(i-1)+phi(i-1)*DT+S(i+1)-phi(i+1)*DT)/2)^2/DT;
-end
+for i = 1:N+1
+    if i == 1
+        beta = beta + (S(i)-(S(i+1)-phi(i+1)*DT))^2/DT;
+    elseif i > 1 && i < N+1
+        beta = beta + (S(i)-(S(i-1)+phi(i-1)*DT+S(i+1)-phi(i+1)*DT)/2)^2/DT;
+    else
+        beta = beta + (S(i)-(S(i-1)+phi(i-1)*DT))^2/DT;
+    end
+end    
 sigma2_out = 1/gamrnd(alpha,1/beta,1,1);
 end
